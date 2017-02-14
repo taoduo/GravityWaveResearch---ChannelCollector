30c214a199c367933114f9e3e46bcb878006de07
Duo Tao <duo.tao@ldas-pcdev1.ligo-wa.caltech.edu>
2017-02-13 21:41:18 -0800

    new search merge

--- LineSearch/single_line/main.m
+++ LineSearch/single_line/main.m
@@ -1,3 +1,4 @@
-line_search('/home/mcoughlin/public_html/O2/LineSearch', 8.7, 10.7, 9.7, '/home/duo.tao/line_9.7', 0);
-line_search('/home/mcoughlin/public_html/O2/LineSearch', 34.7, 36.7, 35.7, '/home/duo.tao/line_35.7', 0);
-line_search('/home/mcoughlin/public_html/O2/LineSearch', 23, 25, 24, '/home/duo.tao/line_24', 0);
+% line_search('/home/mcoughlin/public_html/O2/LineSearch', 8.7, 10.7, 9.7, '/home/duo.tao/line_9.7', 0);
+% line_search('/home/mcoughlin/public_html/O2/LineSearch', 34.7, 36.7, 35.7, '/home/duo.tao/line_35.7', 0);
+% line_search('/home/mcoughlin/public_html/O2/LineSearch', 23, 25, 24, '/home/duo.tao/line_24', 0);
+week_search('/home/mcoughlin/public_html/O2/LineSearch/H1_COH_1165017615_1165622415_SHORT_1_webpage/data', 8.7, 10.7, 9.7, '/home/duo.tao/week_again', 0)
