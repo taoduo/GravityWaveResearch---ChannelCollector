4bde7b303f8902012889cc191306af18dfc339b7
taoduo <taod@carleton.edu>
2017-02-21 19:23:09 -0600

    remove the main

--- LineSearch/doublet/main.m
+++
@@ -1,5 +0,0 @@
-doublet_search('/home/mcoughlin/public_html/O2/LineSearch/', 35, 36, [35.1944, 35.2637], '/home/duo.tao/output_buffer/dou_35', 6);
-doublet_search('/home/mcoughlin/public_html/O2/LineSearch/', 38, 39, [38.6111, 38.6801], '/home/duo.tao/output_buffer/dou_38', 6);
-doublet_search('/home/mcoughlin/public_html/O2/LineSearch/', 40, 41, [40.7217, 40.7897], '/home/duo.tao/output_buffer/dou_40', 6);
-doublet_search('/home/mcoughlin/public_html/O2/LineSearch/', 42, 43, [42.2572, 42.3264], '/home/duo.tao/output_buffer/dou_42', 6);
-
