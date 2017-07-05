af838966ae67d33b3713449c9f7037aa4d4b5d0f
Duo Tao <taod@carleton.edu>
2017-07-05 17:28:12 -0500

    delete jpgs

--- SR/gwinc/comoving_to_luminosity.m
+++
@@ -1,5 +0,0 @@
-function luminosityDistance = comoving_to_luminosity(comovingDistance, z)
-
-    luminosityDistance = comovingDistance.*(1+z);
-    
-end
--- SR/gwinc/luminosity_to_comoving.m
+++
@@ -1,5 +0,0 @@
-function comovingDistance = luminosity_to_comoving(luminosityDistance, z)
-
-    comovingDistance = luminosityDistance./(1+z);
-    
-end
