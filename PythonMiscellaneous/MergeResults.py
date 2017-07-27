8d2146be65e023dc980fa6bcac468d330dd179cb
Duo Tao <taod@carleton.edu>
2017-07-27 11:36:14 -0500

    results merge first draft

---
+++ PythonMiscellaneous/MergeResults.py
@@ -0,0 +1,40 @@
+# code to merge new week coherence results into main results
+# example use:
+# directory structure:
+# ./main/<line_xx>/<week_xx>/<channels>
+# ./new/<line_xx>/<week_xx>/<channels>
+# python ./main ./new
+# This will merge all line search results of ./new into ./main
+
+import argparse
+import os
+import shutil
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("main", help="main results")
+    parser.add_argument("new", help="new results")
+
+
+def merge(main_dir, new_dir):
+    for f in os.listdir(new_dir):
+        if os.path.isdir(os.path.join(new_dir, f)):
+            mdir = os.path.join(main_dir, f)
+            ndir = os.path.join(new_dir, f)
+            if os.path.isdir(mdir):
+                merge(mdir, ndir)
+            else:
+                shutil.copytree(mdir, ndir)
+        else:
+            if os.path.isfile(os.path.join(main_dir, f)):
+                print "File " + new_dir + "/" + f + "exists in main directory and ignored"
+            else:
+                raise Exception("Error with " + new_dir + "/" + f)
+
+
+if __name__ == "__main_":
+    args = parse_args()
+    mainDir = args.main
+    newDir = args.new
+    merge(mainDir, newDir)
