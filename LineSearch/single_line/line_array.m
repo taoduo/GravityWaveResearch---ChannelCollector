e16e64b1024f559b9b9a01c95343bff021c43e8d
Duo Tao <taod@carleton.edu>
2018-01-01 16:37:30 -0600

    line array

--- LineSearch/single_line/line_array.m
+++ LineSearch/single_line/line_array.m
@@ -1,6 +1,6 @@
-function arr = line_array(lines, resolution)
+function arr = line_array(lines, run, observatory, resolution)
 	% Create an array of Line object
-	for i = 1 : length(lines)
-		arr(i) = Line(lines(i), resolution);
+	for i = length(lines) : -1 : 1
+		arr(i) = Line(lines(i), run, observatory, resolution);
 	end
 end
