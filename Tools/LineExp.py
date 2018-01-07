0d30f60d647632a661a9de9491d48aa588465c11
Duo Tao <taod@carleton.edu>
2018-01-06 21:39:48 -0600

    more stats

--- Tools/LineExp.py
+++ Tools/LineExp.py
@@ -307,7 +307,7 @@ def statCalc(path, weeks):
                     tup = channelDict[chn]
                     channelDict[chn] = (tup(0) + 1, tup(1) + sigdict[chn[:-4]])
                 else:
-                    channelDict[chn] = (1, sigdict[chn[:,-4]])
+                    channelDict[chn] = (1, sigdict[chn[:-4]])
     stats['subsystemDict'] = subsysDict
     stats['totalChannel'] = chnTot
     stats['channelStats'] = channelDict
