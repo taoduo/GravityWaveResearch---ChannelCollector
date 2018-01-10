25e7bc810117d1b9ec0a41efcb5a8d48a70f42a2
Duo Tao <taod@carleton.edu>
2018-01-10 00:14:11 -0600

    filter update

--- LineSearch/single_line/channel.m
+++ LineSearch/single_line/channel.m
@@ -11,9 +11,9 @@ function channel(data_path, search, line, output_path)
 	[~, channel_name, ~] = fileparts(data_path);
 	freqGap = freqs(2) - freqs(1);
 	[fp, cp] = search.chopData(data_path, freqs, coh, line); % 2Hz band
-	if (fp == false)
+    if (fp == false)
 		return;
-	end
+    end
 	if (search.filter ~= 0)
 		offset = line.resolution;
         % get the indices
@@ -23,37 +23,38 @@ function channel(data_path, search, line, output_path)
         window_high = min(length(coh), ceil((line.line + search.zoom) / freqGap) + 1);
         % bg calculate
         background = coh([window_low:line_low - 1, line_high + 1:window_high]);
-        bg_std = std(background);
-        bg_omega = bg_std / sqrt(1 - 2 / pi);
 		% filter
+        % get the line part
         fcp = coh(line_low : line_high);
 		filt_max = max(fcp);
         filt_min = min(fcp);
-        md = median(cp);
-        if (md < 0.5)
-            if (filt_max >= bg_omega * search.filter && filt_max >= 0.025)
-                % output the significance as 
-                % channel <tab> significance <tab> confidence
-                sig = filt_max / bg_omega;
-                p = erf(sig / sqrt(2));
-                [weekpath,~,~] = fileparts(output_path);
-                fd = fopen(fullfile(weekpath, 'sig.txt'), 'a');
-                fprintf(fd, strcat(channel_name, '\t', num2str(sig), '\t', num2str(p), '\n'));
-                fclose(fd);
-                output(channel_name, fp, cp, line.line, output_path);
-            end
+        % get params of the model
+        pd = fitdist(background', 'Normal');
+        ctr = pd.mu;
+        stdd = pd.omega;
+        z0 = (0 - ctr) / stdd;
+        z1 = (1 - ctr) / stdd;
+        totarea = vpa(normcdf(vpa(z1)) - normcdf(vpa(z0)));
+        % deviation calculation
+        if (abs(filt_max - ctr) >= abs(filt_min - ctr))
+            maxDev = filt_max - ctr;
         else
-            if (filt_min <= 1 - bg_omega * search.filter && filt_min <= 0.975)
-                % output the significance as 
-                % channel <tab> significance <tab> confidence
-                sig = (1 - filt_min) / bg_omega;
-                p = erf(sig / sqrt(2));
-                [weekpath,~,~] = fileparts(output_path);
-                fd = fopen(fullfile(weekpath, 'sig.txt'), 'a');
-                fprintf(fd, strcat(channel_name, '\t', num2str(sig), '\t', num2str(p), '\n'));
-                fclose(fd);
-                output(channel_name, fp, cp, line.line, output_path);
-            end
+            maxDev = filt_min - ctr;
+        end
+        zdev = maxDev / stdd;
+        if (maxDev > 0)
+            logp = log10(vpa((normcdf(vpa(z1)) - normcdf(vpa(zdev))) / vpa(totarea)));
+        else
+            logp = log10(vpa((normcdf(vpa(zdev)) - normcdf(vpa(z0))) / vpa(totarea)));
+        end
+        if (logp < -16 && maxDev >= 0.025) % p-value should be less than 10^-16
+            % output the significance as 
+            % channel <tab> log p value            
+            [weekpath,~,~] = fileparts(output_path);
+            fd = fopen(fullfile(weekpath, 'sig.txt'), 'a');
+            fprintf(fd, strcat(channel_name, '\t', num2str(ceil(p)), '\n'));
+            fclose(fd);
+            output(channel_name, fp, cp, line.line, output_path);
         end
 	else
 		output(channel_name, fp, cp, line.line, output_path);
