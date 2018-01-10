905a7c92604198dedfc3921132066d50a14f928b
Duo Tao <taod@carleton.edu>
2018-01-10 00:22:18 -0600

    filter update

--- LineSearch/single_line/channel.m
+++ LineSearch/single_line/channel.m
@@ -29,6 +29,7 @@ function channel(data_path, search, line, output_path)
 		filt_max = max(fcp);
         filt_min = min(fcp);
         % get params of the model
+                disp(background)
         pd = fitdist(background', 'Normal');
         ctr = pd.mu;
         stdd = pd.omega;
